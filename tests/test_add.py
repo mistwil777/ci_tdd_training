2605ed8c13bc7161adf2c8f34ccc4ce8b8298d6b
mistwil777 <leroulierwilfried@gmail.com>
2024-10-26 19:10:59 +0200

    test2

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import unittest
 from app.main import add_numbers
     
@@ -7,12 +5,6 @@ class TestAddNumbers(unittest.TestCase):
 
     def test_add_positive_numbers(self):
         self.assertEqual(add_numbers(2, 5), 7)
-
-    def test_add_same_numbers(self):
-        self.assertEqual(add_numbers(2, 2), 4)
-
-    def test_add_negative_and_positive(self):
-        self.assertEqual(add_numbers(-1, 3), 2)
 
 if __name__ == '__main__':
     unittest.main()
